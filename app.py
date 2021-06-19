74e61013d860705e6b7f7597323203318463aab5
raiajit9 <54202447+raiajit9@users.noreply.github.com>
2021-06-20 01:54:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ from werkzeug.utils import secure_filename
 app = Flask(__name__)
 
 # Model saved with Keras model.save()
-MODEL_PATH ='model_inception.h5'
+MODEL_PATH ='C:/Users/WIN/Downloads/Cotton-Disease-Prediction-Deep-Learning-master/model_inception.h5'
 
 # Load your trained model
 model = load_model(MODEL_PATH)
